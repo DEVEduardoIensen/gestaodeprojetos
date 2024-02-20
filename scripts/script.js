632802ae7bb50d29f60211a1072f218f16aac8ea
DEVEeduardoiensen <eduardo.iensen.prestes@escola.pr.gov.br>
2024-02-20 10:28:08 -0300

    idade do dia 20

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let idade = 17
+
+if (idade>18){
+console.log(`${idade} anos -> maior idade!`)
+
+} else if (idade == 18){
+    console.log(`${idade} anos -> esta no limite!`);
+} else {console.log(`${idade} anos -> menor de idade!`)
+
+}
